5c1fd74a7a021b28ba4d3206cf97602d7454e754
Leandra Michely <design.lemi@gmail.com>
2026-02-04 20:22:24 -0300

    add files

---
+++ ColetaFacil/ColetaFacil/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ColetaFacil
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            // Mensagem
+            //DisplayAlert("Informacao", "Vidro", "Fechar");
+
+            // pergunta
+            //if (await DisplayAlert("Informacao", "Vai se cadastrar?", "Não", "Sim") == false)
+            //{
+            //    await DisplayAlert("Informacao", "Executado", "Fechar");
+            //}
+            //else
+            //{
+            //    await DisplayAlert("Informacao", "Nao Executado", "Fechar");
+            //}
+
+        }
+
+        private async void BtVidro_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new VidroPage());
+        }
+        private async void BtEletronicos_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new OleoPage1());
+        }
+        private async void BtOleo_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EletronicosPage());
+        }
+        private async void BtBateria_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new bateriasPage());
+        }
+        private async void Btfornecedor_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProdutosPage());
+        }
+    }
+}
---
+++ ColetaFacil/ColetaFacil/Page1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ColetaFacil
+{
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private async void  BtColeta_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage()); 
+        }
+    }
+}
